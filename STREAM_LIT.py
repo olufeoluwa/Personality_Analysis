452e7ee60cd61511740f36324c40f3f9429b371f
olufeoluwa <odebunmioluwadamilola3@gmail.com>
2025-06-11 13:07:13 +0100

    Create STREAM_LIT.py

---
+++ STREAM_LIT.py
@@ -0,0 +1,17 @@
+import math
+cosine = math.cos(45)
+print(cosine)
+
+power = math.pow(22,4)
+print(power)
+
+
+"""
+add = sum({2,3,4,5,6,7})
+print(add)
+
+number = {2,3,4,5,6,7,8}
+add2 = sum(number)
+print(add2)
+"""
+
